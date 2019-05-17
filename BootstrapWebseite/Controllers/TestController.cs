a6b2c1ee3a943fe29de1f6942d1c90a4d9200d4d
Nico <nico371@schule.bremen.de>
2019-05-17 09:27:59 +0200

    Webseite hinzugefügt.

---
+++ BootstrapWebseite/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using BootstrapWebseite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapWebseite.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Test()
+        {
+            TestModel model = new TestModel()
+            {
+                QuestionCount = 5
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ BootstrapWebseite/Models/TestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BootstrapWebseite.Models
+{
+    public class TestModel
+    {
+        public int QuestionCount { get; set; }
+    }
+}
